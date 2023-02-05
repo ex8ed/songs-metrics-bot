67fc1a17af9e2b9a61248287e6836370b09f731f
ex8ed <an.proshak@gmail.com>
2023-02-05 12:38:36 +0300

    major fixes

--- main.py
+++ main.py
@@ -59,42 +59,42 @@ async def start(message: types.Message):
 # returning mood status callbacks:
 @dp.callback_query_handler(text='btn_mood_drive')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'драйвовое'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
 
 @dp.callback_query_handler(text='btn_mood_melancholy')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'меланхоличное'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
 
 @dp.callback_query_handler(text='btn_mood_freedom')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'свобода'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
 
 @dp.callback_query_handler(text='btn_mood_hurt')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'обида'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
 
 @dp.callback_query_handler(text='btn_mood_sure')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'уверенное'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
 
 @dp.callback_query_handler(text='btn_mood_passionate')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MOOD"] = 'страстное'
     await bot.send_message(message.from_user.id, config.zodiac_step_message, reply_markup=markups.zodiac_chooser)
 
@@ -102,77 +102,77 @@ async def get_mood_drive(message: types.Message):
 # returning zodiac
 @dp.callback_query_handler(text='btn_zodiac_aquarius')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'водолей'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_pisces')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'рыбы'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_taurus')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'телец'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_gemini')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'близнецы'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_cancer')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'рак'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_leo')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'лев'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_virgo')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'дева'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_libra')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'весы'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_scorpio')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'скорпион'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_sagittarius')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'стрелец'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
 
 @dp.callback_query_handler(text='btn_zodiac_capricorn')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["ZODIAC"] = 'козерог'
     await bot.send_message(message.from_user.id, config.event_step_message, reply_markup=markups.event_chooser)
 
@@ -180,49 +180,49 @@ async def get_mood_drive(message: types.Message):
 # returning an actual event
 @dp.callback_query_handler(text='btn_event_date')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'свидание'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_car')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'машина'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_exams')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'сессия'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_breaking_up')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'расставание'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_nostalgia')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'ностальгия'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_being_in_love')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'влюбленность'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
 
 @dp.callback_query_handler(text='btn_event_flirt')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["EVENT"] = 'флирт'
     await bot.send_message(message.from_user.id, config.fos_step_message, reply_markup=markups.fos_chooser)
 
@@ -230,14 +230,14 @@ async def get_mood_drive(message: types.Message):
 # feat or single info
 @dp.callback_query_handler(text='btn_fos_feat')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["FOS"] = 'feat'
     await bot.send_message(message.from_user.id, config.age_step_message, reply_markup=markups.age_chooser)
 
 
 @dp.callback_query_handler(text='btn_fos_single')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["FOS"] = 'single'
     await bot.send_message(message.from_user.id, config.age_step_message, reply_markup=markups.age_chooser)
 
@@ -245,14 +245,14 @@ async def get_mood_drive(message: types.Message):
 # Egor Krid age status:
 @dp.callback_query_handler(text='btn_age_old')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["AGE"] = 'старый'
     await bot.send_message(message.from_user.id, config.clip_step_message, reply_markup=markups.clip_chooser)
 
 
 @dp.callback_query_handler(text='btn_age_new')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["AGE"] = 'новый'
     await bot.send_message(message.from_user.id, config.clip_step_message, reply_markup=markups.clip_chooser)
 
@@ -260,14 +260,14 @@ async def get_mood_drive(message: types.Message):
 # clip choose status:
 @dp.callback_query_handler(text='btn_clip_yes')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["CLIP"] = 'да'
     await bot.send_message(message.from_user.id, config.men_step_message, reply_markup=markups.men_chooser)
 
 
 @dp.callback_query_handler(text='btn_clip_no')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["CLIP"] = 'нет'
     await bot.send_message(message.from_user.id, config.men_step_message, reply_markup=markups.men_chooser)
 
@@ -275,28 +275,28 @@ async def get_mood_drive(message: types.Message):
 # men choose status:
 @dp.callback_query_handler(text='btn_men_romantic')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MEN"] = 'романтичный'
     await bot.send_message(message.from_user.id, generate_playlist(params))
 
 
 @dp.callback_query_handler(text='btn_men_persistent')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MEN"] = 'настойчивый'
     await bot.send_message(message.from_user.id, generate_playlist(params))
 
 
 @dp.callback_query_handler(text='btn_men_energy')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MEN"] = 'заряженный'
     await bot.send_message(message.from_user.id, generate_playlist(params))
 
 
 @dp.callback_query_handler(text='btn_men_muscle')
 async def get_mood_drive(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+    await bot.delete_message(message.from_user.id, message.message.message_id)
     params["MEN"] = 'маскулинный'
     await bot.send_message(message.from_user.id, generate_playlist(params))
 
